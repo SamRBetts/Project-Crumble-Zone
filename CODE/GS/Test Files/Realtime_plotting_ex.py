23a394d4a35f3e25d0fa7fc90320a4d1afd92395
SamRBetts <90115955+SamRBetts@users.noreply.github.com>
2023-10-24 08:45:22 -0400

    Example of realtime plotting with given pressure data
    an example of relevant real-time plotting with given pressure data.

--- CODE/GS/Test Files/Realtime_plotting_ex.py
+++ CODE/GS/Test Files/Realtime_plotting_ex.py
@@ -11,41 +11,75 @@ import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 from random import randint
+import numpy as np
+
+pen = pg.mkPen(color=(255, 0, 0))
+packet_num =0
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
 
-        self.x = list(range(100))  # 100 time points
-        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
+               
+        
 
         self.graphWidget.setBackground('w')
 
-        pen = pg.mkPen(color=(255, 0, 0))
+        
+    
+        
+        
+   
+    def Plot(self,x_arr,y_arr):
+        
+        self.index = 0
+        self.x_arr = x_arr
+        self.y_arr = y_arr
+        
+        self.x =[x_arr[1]]
+        self.y =[y_arr[1]]
+        
         self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
-
-
- # ... init continued ...
+        
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(50)
-        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.setInterval(1000)
+
+        #w.timer.timeout.connect(lambda: w.update_plot_data(time[packet_num],pressure_values[packet_num]))
+        self.timer.timeout.connect(w.update_plot_data)
         self.timer.start()
 
+        
     def update_plot_data(self):
-
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
-
-        self.y = self.y[1:]  # Remove the first
-        self.y.append( randint(0,100))  # Add a new random value.
+      
+        #self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x_arr[self.index])  # Add a new value 1 higher than the last.
+        self.y.append(self.y_arr[self.index])  
+        #self.y = self.y[1:]  # Remove the first
+        #self.y.append(y)  # Add a new random value.
 
         self.data_line.setData(self.x, self.y)  # Update the data.
+        self.index=self.index+1
+
+        print(self.index)
+
+    
+arr = np.loadtxt("cansat_2023_simp.csv", delimiter=",", dtype=str)
+(arr[:,1]) = '2033'
+
+pressure_values = (arr[:,3])
+pressure_values= pressure_values.astype(int)
+time = np.arange(0,arr.shape[0],1)
+    
+
 
 app = QtWidgets.QApplication(sys.argv)
 w = MainWindow()
+w.Plot(time,pressure_values)
+
+
+
 w.show()
 sys.exit(app.exec_())
