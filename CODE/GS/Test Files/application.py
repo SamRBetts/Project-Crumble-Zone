fa18a96367d2b81cc5f1dd59e6ac7b175fc544e3
SamRBetts <90115955+SamRBetts@users.noreply.github.com>
2023-11-01 13:48:09 -0400

    added deployable struts
    Added prototype deployable struts

---
+++ CODE/GS/Test Files/application.py
@@ -0,0 +1,22 @@
+
+#import Printer
+import folder.Printer#import a specific thing from  module, 
+#don't need to call module beforehand
+#import all the needed things
+
+
+def main():
+    
+    folder.Printer.print_test("'sup")
+
+#put all the main execution here 
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
